596249111520edd84a8ef438ee9d380d9e979f4b
unaiiM <unaimatopi@gmail.com>
2024-01-29 13:09:29 +0100

    Started working with parsing the response

--- src/identd/index.ts
+++ src/identd/index.ts
@@ -1,21 +1,27 @@
 import * as net from 'net';
 import * as iconv from 'iconv-lite';
+import charsets from './charset.js';
+import opsys_list from './opsys.js';
 
 export interface Options {
     address : string;
     port? : number; // identd port 113 defualt
-    lport : number; // local port from the machine
-    rport : number; // remote port where the machine is connected
+    server_port : number; // local port from the machine
+    client_port : number; // remote port where the machine is connected
     abort? : boolean; // Abort a connection if they receive 1000 characters without receiving an <EOL>.
 };
 
 // character set from RFC-1340
+// 6193, 23 : USERID : UNIX : stjohns
+// 6195, 23 : ERROR : NO-USER
 export interface Response {
-    ports : string; // x, y
+    server_port : number; // x, y
+    client_port : number;
     status : string; // ERROR / USERID
-    os? : string;
-    user? : string;
+    opsys? : string;
+    userid? : string | Buffer;
     error? : string;
+    charset? : string; // for userid
 }
 
 export default class Identd {
@@ -27,31 +33,16 @@ export default class Identd {
     private readonly COLON : number = 0o72; // :
     // Clients should feel free to abort a connection if they receive 1000 characters without receiving an <EOL>.
     private readonly ABORT_CONNECTION_LENGTH = 1000;
-    private readonly READ_BUFFER_LENGTH : number = this.ABORT_CONNECTION_LENGTH;
+    private readonly READ_BUFFER_LENGTH : number = 256;
     private readonly DEFAULT_CHARSET = 'US-ASCII';
 
     constructor(private options : Options){
+        if(!options.address) throw new Error('Undefined ip!');
+        else if(!options.server_port) throw new Error('Undefined server prot!');
+        else if(!options.client_port) throw new Error('Undefined local port!');
+
         options.port = options.port ?? 113;
         options.abort = options.abort ?? true;
-        if(!options.address) throw new Error('Undefined ip!');
-        else if(!options.lport) throw new Error('Undefined local prot!');
-        else if(!options.rport) throw new Error('Undefined remote port!');
-    };
-
-    private digestResponse(response : string) : Response {
-        let foo : string[] = response.split(':').map((s) => s.trim());
-        let obj : Response = {
-            ports : foo[0],
-            status : foo[1]
-        };
-
-        if(obj.status === 'ERROR') obj.error = foo[2];
-        else if(obj.status === 'USERID'){
-            obj.os = foo[2];
-            obj.user = foo[3];
-        };
-
-        return obj;
     };
 
     /**
@@ -79,7 +70,7 @@ export default class Identd {
                             const res : Buffer = data.subarray(0, index);
                             data = data.subarray(index + 2, data.length); // don't care about if the start is bigger than the end
                             sock.end();
-                            this.handle(res);
+                            resolv(this.parseResponse(res));
                         } else if(this.options.abort && data.length > this.ABORT_CONNECTION_LENGTH) sock.destroy(); // do somthing else 
                         return true;
                     },
@@ -106,10 +97,6 @@ export default class Identd {
         else return -1;
     };
 
-    private handle(buff : Buffer) : void {
-
-    };
-
     /**
      * <request> ::= <port-pair> <EOL>
      * <port-pair> ::= <integer> "," <integer>
@@ -121,6 +108,8 @@ export default class Identd {
     /**
      * <reply> ::= <reply-text> <EOL>
      * <reply-text> ::= <error-reply> | <ident-reply>
+     * <port-pair> ::= <integer> "," <integer>
+     * <integer> ::= 1*5<digit> ; 1-5 digits.
      * <error-reply> ::= <port-pair> ":" "ERROR" ":" <error-type>
      * <ident-reply> ::= <port-pair> ":" "USERID" ":" <opsys-field> ":" <user-id>
      * <error-type> ::= "INVALID-PORT" | "NO-USER" | "UNKNOWN-ERROR" | "HIDDEN-USER" |  <error-token>
@@ -150,7 +139,69 @@ export default class Identd {
      * --
      * only the beggining and ending white spaces will be taken as a part of the userid
      */
-    private parseResponse(res : string) {
-        // to do
+    private parseResponse(buff : Buffer) : Response {
+        // port pair
+        let response : Partial<Response> = {};
+        let arr : Buffer[] = this.splitBuffer(buff, this.COLON);
+
+        let str : string = this.defaultDecode(arr[0]);
+        let foo : string[] = str.split(',');
+        response.server_port = parseInt(foo[0]); // trim is alredy done it by parseInt func
+        response.client_port = parseInt(foo[1]);
+
+        str = this.defaultDecode(arr[1]).trim();
+        response.status = str;
+
+        str = this.defaultDecode(arr[2]).trim();
+        if(response.status === 'USERID'){
+            foo = str.split(',');
+            response.opsys = foo[0].trim();
+
+            if(foo[1]){
+                response.charset = foo[1].trim();
+                response.userid = arr[3];
+            }else {
+                response.userid = this.defaultDecode(arr[3]);
+            };
+        }else if(response.status === 'ERROR'){
+            response.error = str;
+        }else {
+            // some error log
+        };
+
+        return <Response> response;
     };
+
+    private defaultDecode(buff : Buffer) : string {
+        return iconv.decode(buff, this.DEFAULT_CHARSET)
+    };
+
+    private splitBuffer(buff : Buffer, char : number) : Buffer[] {
+        let arr : Buffer[] = [];
+        let index : number;
+        while((index = buff.indexOf(char)) !== -1){
+            arr.push(buff.subarray(0, index));
+            buff = buff.subarray(index + 1);
+        };
+
+        if(buff.length > 0) arr.push(buff.subarray(0));
+        return arr;
+    };
+
+    // <port-on-server> , <port-on-client>
+    // where <port-on-server> is the TCP port (decimal) on the target (where
+    // the "ident" server is running) system, and <port-on-client> is the
+    // TCP port (decimal) on the source (client) system.
+    private makeRequest(server_port : number, client_port : number) : string {
+        return `${server_port}, ${client_port}${this.EOL}`;
+    };
+
+    private isValidOS(opsys : string) : boolean {
+        return opsys === 'OTHER' || opsys_list.indexOf(opsys) !== -1;
+    };
+
+    private isValidCharset(charset : string) : boolean {
+        return charsets.indexOf(charset) !== -1;
+    };
+    
 };
